cc4934fb12c699ea1fa6eb47ae3db38a27ef5bb9
wnsgus <ggp032177@naver.com>
2021-10-07 21:52:30 +0900

    edit pd

--- src/function.py
+++ src/function.py
@@ -4,7 +4,6 @@ from scipy.io import savemat
 from scipy.signal import find_peaks
 import pandas as pd
 
-
 def loadMat(path,name):
     mat_file1 = io.loadmat(path)
     data = mat_file1[name]
@@ -24,16 +23,16 @@ def grpstats(epi1msk,pgd1):
     max = np.max(pgd1)
     min = np.min(pgd1)
     result = np.empty((max-min+1 ,epi1msk.shape[1]),object)
-    time = np.zeros(max-min+1)
+    nVtxInBin = np.zeros(max-min+1) # the number of vertices in each bin (1->70)
     for i in range(pgd1.size):
-        time[pgd1[i][0]-1] +=1
+        nVtxInBin[pgd1[i][0]-1] +=1
         if(result[pgd1[i][0]-1][0] == None):
             temp = epi1msk[i,:]
         else:
             temp = result[pgd1[i][0]-1,:] + epi1msk[i,:]
         result[pgd1[i][0]-1,:] = temp
     for j in range(max-min+1):
-        result[j,:] = result[j,:]/ time[j]
+        result[j,:] = result[j,:]/ nVtxInBin[j]
     return result
 
 
@@ -47,17 +46,18 @@ def size(data,axis=0):
         return data.shape[axis]
 
 
-def findPeaks(array):
-    locs= find_peaks(array)[0]
+def findPeaks(data):
+    locs= find_peaks(data)[0]
     peak = np.array([])
     for i in range(locs.size):
-        peak = np.append(peak,array[locs[i]]) 
+        peak = np.append(peak,data[locs[i]]) 
     return peak,locs
 
+
 def inpaintNans(array):
+    if np.isnan(array[0]):
+        array[0] = array[1]
+    if np.isnan(array[-1]):
+        array[-1] = array[-2]
     df = pd.DataFrame(array.astype(np.float)).interpolate()
-    return df.T.to_numpy()
-    
-if __name__ == '__main__':
-    a = np.array([0.0, np.nan, -1.0, 1.0,np.nan, 2.0, np.nan, np.nan,2.0, 3.0, np.nan, 9.0])
-    print(inpaintNans(a))
+    return df.to_numpy()
--- src/pd.py
+++ src/pd.py
@@ -5,37 +5,34 @@ from scipy.signal import find_peaks
 from function import *
 
 if __name__ == '__main__':
-
-    path ="/home/cjh/skku/2021_second_semester/FMRI/project/Data"
+    path ="/hdd_ext/sdc/class/2021-2/advancedfMRI/data/human/test"
    
-
     epi = loadMat(path+"/epi1msk","epi1msk")
-
     # calculate delay profile across time segments: idx_dly
 
     gls_neg_pk,locs = findPeaks(epi.mean(axis=0)) # cut the global mean signal into time segments - Figure 2A upper graph
-
     idx_dly = np.zeros((size(epi,0),size(locs,1)-1),object) # vertices * segments
 
-    for li in range(0,size(locs,1)-1):
+    for li in np.arange(locs.shape[0]-1):
         tmp_prin = np.array([]) 
         tmp_prin = epi[:,locs[li]:locs[li+1]] # get one time segment
 
         # locate max peaks in each bin/electrode
-        for lj in (0,size(epi,1)):
+        for lj in np.arange(epi.shape[0]):
             tmp2_prin = tmp_prin[lj,:] # get one vertex in one time segment
             pks_prin, a_prin = findPeaks(tmp2_prin) # and find peak of that vertex - Figure 2A Delay profile #1
             # pks_prin: the value of each peak
             # a_prin: index of the peak
+
             thre1 = 0
 
             # find location of largest peak in each vertex
             if a_prin.size == 0: # no peak
-                idx_dly[lj,li] = np.NaN; 
+                idx_dly[lj,li] = np.NaN
 
             elif size(a_prin,1) > 1: # more than one peaks exist
                 valmax1= np.max(pks_prin) # pick just one peak that has the highest value
-                id_prin = np.argmin(pks_prin) 
+                id_prin = np.argmin(pks_prin)
 
                 if valmax1 <= thre1:
                     idx_dly[lj,li] = np.NaN; # if the peak value is lower than the threshold value, make it nan
@@ -45,28 +42,34 @@ if __name__ == '__main__':
 
             elif pks_prin > thre1: # one peak, and above the threshold -> save the index
                 idx_dly[lj,li] = a_prin[0]
+
             else :
                 idx_dly[lj,li] = np.NaN
-
     ## applying SVD on delay profiles to calculate the principal delay profile: pd
-        
+      
     # fill nan
     seg_all = idx_dly
     seg_all2 = np.array([])
-    for l in range(0,size(seg_all,1)):
+
+    for li in np.arange(seg_all.shape[1]):
         temp1 = seg_all[:,li]
-        if np.sum(np.isnan(temp1.astype(np.float))) < size(seg_all,0) * 0.2:
-            if seg_all2.size == 0:
+
+        if np.sum(np.isnan(temp1.astype(np.float))) < seg_all.shape[0] * 0.2:
+            if seg_all2.shape[0] == 0:
                 seg_all2 = inpaintNans(temp1)
             else:
-                seg_all2 = np.concatenate((seg_all2,inpaintNans(temp1)),axis=0)
-
-    delay_test3 = seg_all2 - np.tile(np.mean(seg_all2),(size(seg_all,1),1))
+                seg_all2 = np.concatenate((seg_all2, inpaintNans(temp1)),axis=1)
+    delay_test3 = seg_all2 - np.tile(np.mean(seg_all2),seg_all2.shape[1])
+    
+    delay_test3 = delay_test3.astype(np.float)
+    delay_test3 = delay_test3[~np.isnan(delay_test3)]
+    
+    delay_test3_small = delay_test3[:10000, :10000]
     U,S,V = np.linalg.svd(delay_test3, full_matrices = True)
-
-    pd = U[:, 1]
+    pd1 = U[:, 1]
 
     # variance explained for each components
+    
     temp2 = np.diag(S)
     var_exp = temp2**2/np.sum(temp2**2)
     print(var_exp)
